d6870b5b0df376e8234c8bf5f47de09214dbe275
ArkadiuszSnaglewski <arkadiusz.snaglewski@gmail.com>
2020-09-07 15:00:54 +0200

    next problem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,8 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  config.serve_static_assets = true
+  #dodane 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
@@ -89,6 +91,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.assets.digest = true
+  #dodane
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
